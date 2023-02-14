f72e8d3bb8ac9ff87d45217cd953107794d250ec
Bioneisme <bioneisme@gmail.com>
2023-02-14 13:02:26 +0600

    Added default route and tsc start

--- api/bot/bot.ts
+++ api/bot/bot.ts
@@ -2,9 +2,17 @@ import {Telegraf} from "telegraf";
 import {BOT} from "../config/settings.js";
 import commands from "./commands.js";
 import menu from "./menu.js";
+import logger from "../config/logger.js";
 
 export const bot = new Telegraf(BOT.token);
+export const __initBot = () => {};
 
 bot.use(commands);
 
-bot.telegram.setMyCommands(menu);
+bot.telegram.setMyCommands(menu).then(() => {
+    logger.info(`[BOT] commands set`);
+});
+
+bot.launch().then(() => {
+    logger.info(`[BOT] started`);
+});
--- api/index.ts
+++ api/index.ts
@@ -7,8 +7,9 @@ import {writeDateLogging, logging} from "./middlewares/loggingMiddleware.js";
 import dataRoute from "./routes/dataRoute.js";
 import gptRoute from "./routes/gptRoute.js";
 import mongoose from "mongoose";
-import {bot} from "./bot/bot.js";
+import {__initBot} from "./bot/bot.js";
 
+__initBot();
 const app: Application = express();
 
 app.use(express.json());
@@ -17,12 +18,16 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(writeDateLogging);
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static('uploads'));
 app.use("/api/data", dataRoute);
 app.use("/api/gpt", gptRoute);
+app.use(function(req, res){
+    res.json({});
+});
 app.use(logging);
 
 app.listen(SERVER_PORT, async () => {
+    logger.info(`Server Started on port ${SERVER_PORT}`);
     mongoose.connect(DB_URI, {}, function (err) {
         if (err) {
             logger.error(`MongoDB connection error: ${err}`);
@@ -30,7 +35,4 @@ app.listen(SERVER_PORT, async () => {
             logger.info(`Connected to MongoDB`);
         }
     });
-
-    await bot.launch();
-    logger.info(`Server Started on port ${SERVER_PORT}`);
 });
